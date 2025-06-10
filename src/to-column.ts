3abeca431890e14e5a6e46c1c59c52f8c2c0c761
Baptiste Pillot <baptiste@pillot.fr>
2025-06-10 12:39:48 +0200

    Add former names + relational columns; improve id definition

--- src/to-column.ts
+++ src/to-column.ts
@@ -1,9 +1,11 @@
-import { Column }             from '@itrocks/table-schema'
-import { Type }               from '@itrocks/table-schema'
+import { isAnyType }          from '@itrocks/class-type'
 import { Type as TargetType } from '@itrocks/class-type'
+import { formerNameOf }       from '@itrocks/former-name'
 import { ReflectClass }       from '@itrocks/reflect'
 import { ReflectProperty }    from '@itrocks/reflect'
 import { toColumn }           from '@itrocks/rename'
+import { Column }             from '@itrocks/table-schema'
+import { Type }               from '@itrocks/table-schema'
 import { ToType }             from './to-type'
 
 export class ToColumn
@@ -12,7 +14,7 @@ export class ToColumn
 
 	convertMultiple<T extends object = object>(type: TargetType<T>): Column[]
 	{
-		const columns = [new Column('id', new Type('integer'), { autoIncrement: true })]
+		const columns = [new Column('id', new Type('integer', { signed: false, zeroFill: false }), { autoIncrement: true })]
 		for (const property of new ReflectClass(type).properties) {
 			const column = this.convertProperty(property)
 			if (!column) continue
@@ -24,8 +26,14 @@ export class ToColumn
 	convertProperty<T extends object>(property: ReflectProperty<T>): Column | undefined
 	{
 		const type = this.toType.convert<T>(property)
-		if (!type) return
-		return new Column(toColumn(property.name), type)
+		return type
+			? new Column(toColumn(property.name), type, {
+				default:     property.defaultValue,
+				formerNames: formerNameOf(property.class.type, property.name)
+			})
+			: isAnyType(property.type)
+			? new Column(toColumn(property.name) + '_id', new Type('integer', { signed: false, zeroFill: false }))
+			: undefined
 	}
 
 }
